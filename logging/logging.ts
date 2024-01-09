b1a774acb1eff3c00476660e0c5454bf39a10c5f
tbrown224 <131697891+tbrown224@users.noreply.github.com>
2024-01-09 14:35:54 +0000

    Fpad 1171 refactor fraud logger to use generic methods (#3)
    * fpad-984-update-logging-npm
    
    * fpad-984-update-logging-api
    
    * fpad-984-update-logger
    
    * fpad-984-add-xml-comments
    
    * fpad-984-version-increment
    
    * fpad-984-version-increment
    
    * fpad-984-version-increment
    
    * fpad-984-version-increment
    
    * fpad-984-add-ts-config
    
    * fpad-984-add-build-step
    
    * fpad-984-update-ts-config
    
    * fpad-984-update-ts-config
    
    * fpad-984-update-dependencies
    
    * fpad-984-address-pr-comments
    
    * fpad-1171-generic-logging-version-increment
    
    * fpad-1171-update-version
    
    * fpad-1171-temp-update-unit-tests
    
    * fpad-1171-update-unit-tests
    
    * fpad-1171-version-increment
    
    * refactored logging.ts
    
    * fix unit tests
    
    * updated dependencies
    
    * Update logging/logging.ts
    
    Co-authored-by: Dan Lewis <daniel.lewis@digital.cabinet-office.gov.uk>
    
    * Update logging/logging.ts
    
    Co-authored-by: Dan Lewis <daniel.lewis@digital.cabinet-office.gov.uk>
    
    * removed redundant code and added new functions
    
    * Added tests and additional functions
    
    * version bump
    
    ---------
    
    Co-authored-by: Andrew Brough <andrew.brough@digital.cabinet-office.gov.uk>
    Co-authored-by: Dan Lewis <daniel.lewis@digital.cabinet-office.gov.uk>

--- logging/logging.ts
+++ logging/logging.ts
@@ -2,150 +2,37 @@ import { Logger } from "@aws-lambda-powertools/logger";
 import { LogEvents } from "./log-events";
 import { MetricUnits, Metrics } from "@aws-lambda-powertools/metrics";
 import { Tracer } from "@aws-lambda-powertools/tracer";
+import { LogItemExtraInput } from "@aws-lambda-powertools/logger/lib/types";
 
 export const fraudTracer = new Tracer();
 
-export class FraudLogger {
-  constructor(
-    public logger: Logger,
-    public metrics: Metrics,
-    public environment?: string
-  ) {
-    if (environment === "development") this.logger.setLogLevel("DEBUG");
+export class FraudLogger extends Logger {
+  public metrics: Metrics;
+  constructor(serviceName: string, namespace?: string) {
+    super();
+    this.metrics = new Metrics({
+      serviceName: serviceName,
+      namespace: namespace,
+    });
   }
-
-  /**
-   * Send Started Processing Event log
-   *
-   * @param messageId
-   */
-  logStartedProcessing = (messageId?: string): void => {
-    this.logger.info(LogEvents.StartedProcessing, { messageId });
-    this.metrics.addMetric(LogEvents.StartedProcessing, MetricUnits.Count, 1);
-  };
-
-  /**
-   *  Send Successfully Processed Event log
-   *
-   * @param previousMessageId
-   * @param newMessageId
-   */
-  logSuccessfullyProcessed = (
-    previousMessageId?: string,
-    newMessageId?: string
-  ): void => {
-    this.logger.info(
-      LogEvents.SuccessfullyProcessed,
-      { previousMessageId },
-      { newMessageId }
-    );
-    this.metrics.addMetric(
-      LogEvents.SuccessfullyProcessed,
-      MetricUnits.Count,
-      1
-    );
-  };
-
-  /**
-   * Send successful JWS ecryption event log
-   *
-   * @param messageId - The unique identifier of the message that was successfully decrypted.
-   */
-  logJWSVerifySuccess = (messageId: string): void => {
-    this.logger.info(LogEvents.JWSVerifySuccess, { messageId });
-    this.metrics.addMetric(LogEvents.JWSVerifySuccess, MetricUnits.Count, 1);
-  };
-
-  /**
-   * Send Error Event log
-   *
-   * @param messageId
-   * @param error
-   */
-  logErrorProcessing = (messageId?: string, error?: any): void => {
-    this.logger.error(LogEvents.ErrorProcessing, { messageId }, { error });
-    this.metrics.addMetric(LogEvents.ErrorProcessing, MetricUnits.Count, 1);
-  };
-
-  /**
-   *  Send Successfully Generated Individual SET Event log
-   *
-   * @param messageId
-   */
-  logSETBatchProcess = ([logMessage, successfulMessageIds, failedMessageIds]: [
-    LogEvents,
-    string[],
-    string[],
-  ]): void => {
-    this.logger.info(
-      logMessage,
-      { successfulMessageIds },
-      { failedMessageIds }
-    );
-    this.metrics.addMetric(logMessage, MetricUnits.Count, 1);
-  };
-
-  /**
-   *  Send Failed Event Record Processing log
-   *
-   * @param messageId
-   */
-  logFailedMessageProcessing = ([logMessage, failedMessageIds]: [
-    string,
-    { itemIdentifier: string }[],
-  ]): void => {
-    this.logger.info(logMessage, { failedMessageIds });
-    this.metrics.addMetric(logMessage, MetricUnits.Count, 1);
-  };
-
-  /**
-   * Send Generic Log Event log
-   *
-   * @param messageId
-   * @param error
-   */
-  logMessage = (logMessage: string): void => {
-    this.logger.info(logMessage);
-    this.metrics.addMetric(logMessage, MetricUnits.Count, 1);
-  };
-
-  /**
-   * Send Generic Debug Log Event log
-   *
-   * Only when logger level is equal to 'DEBUG'
-   *
-   * @param message
-   */
-  logDebug = (message: string): void => {
-    if (this.logger.getLevelName() === "DEBUG") {
-      this.logger.debug(message);
-    }
-  };
-
-  /**
-   * Log a successful JWS sign
-   * @param jws the completed JWS, including signature
-   */
-  logJWSSignSuccess = (jws: string, messageId: string): void => {
-    this.logger.info(LogEvents.JWSSignSuccess, { jws }, { messageId });
-    this.metrics.addMetric(LogEvents.JWSSignSuccess, MetricUnits.Count, 1);
-  };
-
-  /**
-   *  Send Successfully Generated Individual SET Event log
-   *
-   * @param messageId
-   */
-  logSETBatchGeneration = ([
-    logMessage,
-    successfulMessageIds,
-    failedMessageIds,
-  ]: [LogEvents, string[], string[]]): void => {
-    this.logger.info(
-      logMessage,
-      { successfulMessageIds },
-      { failedMessageIds }
-    );
-    this.metrics.addMetric(logMessage, MetricUnits.Count, 1);
-  };
+  debugWithMetrics = (input: string, metric: LogEvents, ...extraInput: LogItemExtraInput): void => {
+    super.debug(input, ...extraInput);
+    this.metrics.addMetric(metric, MetricUnits.Count, 1);
+  }
+  infoWithMetrics = (input: string, metric: LogEvents, ...extraInput: LogItemExtraInput): void => {
+    super.info(input, ...extraInput);
+    this.metrics.addMetric(metric, MetricUnits.Count, 1);
+  }
+  warnWithMetrics = (input: string, metric: LogEvents, ...extraInput: LogItemExtraInput): void => {
+    super.warn(input, ...extraInput);
+    this.metrics.addMetric(metric, MetricUnits.Count, 1);
+  }
+  errorWithMetrics = (input: string, metric: LogEvents, ...extraInput: LogItemExtraInput): void => {
+    super.error(input, ...extraInput);
+    this.metrics.addMetric(metric, MetricUnits.Count, 1);
+  }
+  criticalWithMetrics = (input: string, metric: LogEvents, ...extraInput: LogItemExtraInput): void => {
+    super.critical(input, ...extraInput);
+    this.metrics.addMetric(metric, MetricUnits.Count, 1);
+  }
 }
