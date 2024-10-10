7b8118f9019f58d02867fd44c8f78ca6a2a1662a
James Hodgson <james.hodgson@digital.cabinet-office.gov.uk>
2024-10-10 11:12:21 +0100

    Added PartialSQSBatchGenerated log event

--- logging/log-events.ts
+++ logging/log-events.ts
@@ -4,6 +4,7 @@ export enum LogEvents {
   ErrorProcessing = 'Error Processing',
   JWSVerifySuccess = 'Successfully verified JWS',
   FullSQSBatchGenerated = 'Successfully sent all SQS Batch messages',
+  PartialSQSBatchGenerated = 'Partially sent all SQS Batch messages',
   FailedSQSBatchGenerated = 'Failed to send all attempted SQS Batch messages',
   PartialSETBatchGenerated = 'SET Batch Generation Partially Successful',
   JWSSignSuccess = 'Successfully signed JWS',
