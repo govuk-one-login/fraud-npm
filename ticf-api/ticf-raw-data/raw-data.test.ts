202dc8577358258e3e50c1f4837c5cdec205bb32
Mark Bennett <mark.bennett@digital.cabinet-office.gov.uk>
2025-10-07 09:52:58 +0100

    FPAD-6214: Fixed issue with TICF API package build (#35)
    Co-authored-by: “Mel <mel.philpot@digital.cabinet-office.gov.uk>
    Co-authored-by: Dominic Taylor <dominic.taylor1@digital.cabinet-office.gov.uk>

---
+++ ticf-api/ticf-raw-data/raw-data.test.ts
@@ -0,0 +1,47 @@
+import { RawDataApiRequest, RawDataApiResponse, RawDataRequestStatus, RawDataRequestType } from './raw-data';
+
+describe('RawDataInterface', () => {
+  test('request should be defined', () => {
+    const req: RawDataApiRequest = {
+      requestType: RawDataRequestType.PASSPORT,
+      requestOriginator: 'requestOriginator',
+      subjectId: 'subjectId',
+      requestField: {
+        name: 'name',
+        value: 'value',
+      },
+    };
+
+    expect(req).toStrictEqual({
+      requestType: RawDataRequestType.PASSPORT,
+      requestOriginator: 'requestOriginator',
+      subjectId: 'subjectId',
+      requestField: {
+        name: 'name',
+        value: 'value',
+      },
+    });
+  });
+
+  test('response should be defined', () => {
+    const req: RawDataApiResponse = {
+      requestType: RawDataRequestType.PASSPORT,
+      requestId: 'requestId',
+      requestStatus: RawDataRequestStatus.SUCCESSFUL,
+      responseField: {
+        name: 'name',
+        value: 'value',
+      },
+    };
+
+    expect(req).toStrictEqual({
+      requestType: RawDataRequestType.PASSPORT,
+      requestId: 'requestId',
+      requestStatus: RawDataRequestStatus.SUCCESSFUL,
+      responseField: {
+        name: 'name',
+        value: 'value',
+      },
+    });
+  });
+});
